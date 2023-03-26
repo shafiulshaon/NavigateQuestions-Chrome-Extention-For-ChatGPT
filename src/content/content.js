f072afcc8c6edb7c084c3c61b2f77e3770dc23f4
shafiul <shafiulshaon@gmail.com>
2023-03-26 12:00:09 +0900

    Add event listener to show tooptip when hover over a question number

--- src/content/content.js
+++ src/content/content.js
@@ -13,11 +13,31 @@ sendButton.addEventListener('click', () => {
   questionElement.innerText = questionNumber++;
   inputBox.parentNode.insertBefore(questionElement, inputBox.nextSibling);
 
-  //Update the question numbers for all existing question numbers whenever a new question is submitted
+  //Update the question numbers for all existing question numbers whenever a new question is submitted.
   const questionNumbers = document.querySelectorAll('.question-number');
   questionNumbers.forEach((numberElement, index) => {
     numberElement.innerText = index + 1;
   });
+
+  //Add event listener to each question number element that shows a tooltip when the user hovers over it. 
+  questionElement.addEventListener('mouseenter', event => {
+    const questionIndex = parseInt(event.target.innerText) - 1;
+    const questions = document.querySelectorAll('.bubble:not(.you) .text');
+    const question = questions[questionIndex].innerText;
+    
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.innerText = question;
+    event.target.parentNode.appendChild(tooltip);
+
+    tooltip.style.top = `${event.target.offsetTop + event.target.offsetHeight}px`;
+    tooltip.style.left = `${event.target.offsetLeft + event.target.offsetWidth / 2 - tooltip.offsetWidth / 2}px`;
+
+    event.target.addEventListener('mouseleave', () => {
+      tooltip.remove();
+    });
+  });
+
 });
 
 inputBox.addEventListener('input', () => {
